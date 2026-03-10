68be297694c666882ae59d342e2e7b80737a21ba
xc8pt <brolike636@gmail.com>
2026-03-10 10:58:31 +0300

    Step 4: Add words data and game logic

---
+++ app/src/main/java/com/abdulin/unscramblegame/data/WordsData.kt
@@ -0,0 +1,12 @@
+package com.abdulin.unscramblegame.data
+
+// Максимальное количество слов в игре
+const val MAX_NO_OF_WORDS = 10
+// Количество очков за правильный ответ
+const val SCORE_INCREASE = 20
+// Список слов для игры
+val allWords: List<String> = listOf(
+    "android", "kotlin", "compose", "jetpack", "viewmodel", "coroutine", "firebase",
+    "material", "database", "network", "retrofit", "room", "navigation", "lifecycle",
+    "dependency", "injection", "testing", "debugging", "gradle", "studio"
+)
--- app/src/main/java/com/abdulin/unscramblegame/ui_model/GameViewModel.kt
+++ app/src/main/java/com/abdulin/unscramblegame/ui_model/GameViewModel.kt
@@ -1,19 +1,46 @@
 package com.abdulin.unscramblegame.ui_model
 
+
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import com.abdulin.unscramblegame.data.GameUiState
+import com.abdulin.unscramblegame.data.allWords
 
 class GameViewModel: ViewModel() {
     private val _uiState = MutableStateFlow(GameUiState())
     val uiState: StateFlow<GameUiState> = _uiState.asStateFlow()
 
+    private lateinit var currentWord: String
+    private var usedWords: MutableSet<String> = mutableSetOf()
+
     init {
         resetGame()
     }
 
-    fun resetGame() {}
-        // TODO:
+    fun resetGame() {
+        usedWords.clear()
+        _uiState.value = GameUiState(
+            currentScrambledWord = pickRandomWordAndShuffle()
+        )
+    }
+    private fun shuffleCurrentWord(word: String): String {
+        val tempWord = word.toCharArray()
+        tempWord.shuffle()
+        while (String(tempWord) == word) {
+            tempWord.shuffle()
+        }
+        return String(tempWord)
+    }
+
+    private fun pickRandomWordAndShuffle(): String {
+        currentWord = allWords.random()
+        while (usedWords.contains(currentWord)) {
+            currentWord = allWords.random()
+        }
+
+        usedWords.add(currentWord)
+        return shuffleCurrentWord(currentWord)
+    }
 }
