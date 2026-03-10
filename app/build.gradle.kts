1ddf937d662e1f04a388ed9edfe98b5797b6dde7
xc8pt <brolike636@gmail.com>
2026-03-10 10:32:41 +0300

    Step 1: Add ViewModel dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,4 +57,6 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.10.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.10.0")
 }
